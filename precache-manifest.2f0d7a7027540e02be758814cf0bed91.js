211abd4b33043fd99994dfd0c99305602473dce9
unknown <piyush96arora@gmail.com>
2019-02-28 22:41:44 +0530

    Updates

---
+++ precache-manifest.2f0d7a7027540e02be758814cf0bed91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bac4d473f46cd8453361",
+    "url": "/piyush96arora1.github.io/bookgram-react-ts/static/js/runtime~main.bac4d473.js"
+  },
+  {
+    "revision": "b7859213cae90c9be2c9",
+    "url": "/piyush96arora1.github.io/bookgram-react-ts/static/js/main.b7859213.chunk.js"
+  },
+  {
+    "revision": "f99526a56d5b41fcc67f",
+    "url": "/piyush96arora1.github.io/bookgram-react-ts/static/js/1.f99526a5.chunk.js"
+  },
+  {
+    "revision": "b7859213cae90c9be2c9",
+    "url": "/piyush96arora1.github.io/bookgram-react-ts/static/css/main.b94630f7.chunk.css"
+  },
+  {
+    "revision": "a317cbaffc3f91b3237647c2efaad935",
+    "url": "/piyush96arora1.github.io/bookgram-react-ts/index.html"
+  }
+];
